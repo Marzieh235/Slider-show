f50fdf3505ce7927cf4d592a5a141fe25905fa66
Marzieh mozaffari <121443046+Marzieh235@users.noreply.github.com>
2023-01-05 02:12:10 +0330

    buttons for slider

--- slider.js
+++ slider.js
@@ -21,14 +21,41 @@ class slider {
         this.sliders = [...sliderElement.children].filter(elm => elm.classList.contains(slideClass))
     }
 
-    createNextAndPrevBtns(){
-        let {el : sliderElement }=this.options;
+    createNextAndPrevBtns() {
+        let { el : sliderElement } = this.options;
+
+        sliderElement.insertAdjacentHTML('beforeend' , `
+            <a class="next">&#10095;</a>
+            <a class="prev">&#10094;</a>
+        `);
+
+        sliderElement.querySelector('.next').addEventListener('click' , () => this.incrementSlide())
+        sliderElement.querySelector('.prev').addEventListener('click' , () => this.decrementSlide())
     }
 
-    createDots(){
+    incrementSlide = () => this.showSlides(this.slideIndex += 1)
+    decrementSlide = () => this.showSlides(this.slideIndex -= 1)
+
+    createDots() {
+        let { el : sliderElement} = this.options;
+
+        let dotElements = [...this.sliders].map((slider , index) => `<span class="dot" data-slide="${index+1}"></span>`)
+
+        let dots = document.createElement('div')
+        dots.classList.add('dots');
+        dots.innerHTML = `${dotElements.join('')}`
+
+        sliderElement.after(dots);
+
+        this.dots = dots.querySelectorAll('.dot');
+        this.dots.forEach(dot => dot.addEventListener('click' , e => console.log(e)))
 
     }
-    showSlides(){
-        
+
+    showSlides(number) {
+        if(number > this.sliders.length) this.slideIndex = 1;
+        if(number < 1 ) this.slideIndex = this.sliders.length
+
+       
     }
 }
